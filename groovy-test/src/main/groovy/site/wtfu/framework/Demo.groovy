f3fc6bd29dfbe860c62ad4032024d2bfd1434e9a
XHS-12302 <xhsgg12302@gmail.com>
2024-01-11 04:40:28 +0800

    perf: commit something

--- groovy-test/src/main/groovy/site/wtfu/framework/Demo.groovy
+++ groovy-test/src/main/groovy/site/wtfu/framework/Demo.groovy
@@ -14,7 +14,20 @@ class Demo {
     }
 
     @Test
-    void test(){
+    void test() {
         print 'xhsgg12302@126.com'
+    }
+
+    @Test
+    void testJavaFlag() {
+        flag:
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("outloopByBreakLikeGoto==j==" + j);
+                if (j == 2) continue flag;
+            }
+            System.out.println("outloopByBreakLikeGoto==i====>>>" + i);
+        }
+        System.out.println("outloopByBreakLikeGoto======>>>END");
     }
 }
--- springboot/src/main/java/site/wtfu/framework/controller/TestController.java
+++ springboot/src/main/java/site/wtfu/framework/controller/TestController.java
@@ -3,6 +3,8 @@ package site.wtfu.framework.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author <a href="mailto:"
  */
@@ -13,5 +15,11 @@ public class TestController {
     @RequestMapping(value = "/demo")
     public String sayHello(){
         return "hello world";
+    }
+
+    @RequestMapping(value = "/request")
+    public String test(HttpServletRequest request){
+        System.out.println("OK");
+        return "OK";
     }
 }
